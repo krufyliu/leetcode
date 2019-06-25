0adb4d510faca91734b9468750a02a7f56235051
liujun <lj90214@gmail.com>
2019-06-25 13:05:43 +0800

    new solutions for 44

--- 44.wildcard-matching_test.go
+++ 44.wildcard-matching_test.go
@@ -17,151 +17,151 @@ func Test_isMatch(t *testing.T) {
 		args args
 		want bool
 	}{
-		/*
-			{
-				name: "case 1",
-				args: args{
-					s: "aa",
-					p: "a",
-				},
-				want: false,
+		
+		{
+			name: "case 1",
+			args: args{
+				s: "aa",
+				p: "a",
 			},
-			{
-				name: "case 2",
-				args: args{
-					s: "aa",
-					p: "*",
-				},
-				want: true,
+			want: false,
+		},
+		{
+			name: "case 2",
+			args: args{
+				s: "aa",
+				p: "*",
 			},
-			{
-				name: "case 3",
-				args: args{
-					s: "cb",
-					p: "*a",
-				},
-				want: false,
+			want: true,
+		},
+		{
+			name: "case 3",
+			args: args{
+				s: "cb",
+				p: "*a",
 			},
-			{
-				name: "case 4",
-				args: args{
-					s: "adceb",
-					p: "*a*b",
-				},
-				want: true,
+			want: false,
+		},
+		{
+			name: "case 4",
+			args: args{
+				s: "adceb",
+				p: "*a*b",
 			},
-			{
-				name: "case 5",
-				args: args{
-					s: "acdcb",
-					p: "a*c?b",
-				},
-				want: false,
+			want: true,
+		},
+		{
+			name: "case 5",
+			args: args{
+				s: "acdcb",
+				p: "a*c?b",
 			},
-			{
-				name: "case 6",
-				args: args{
-					s: "",
-					p: "*",
-				},
-				want: true,
+			want: false,
+		},
+		{
+			name: "case 6",
+			args: args{
+				s: "",
+				p: "*",
 			},
-			{
-				name: "case 7",
-				args: args{
-					s: "",
-					p: "***",
-				},
-				want: true,
+			want: true,
+		},
+		{
+			name: "case 7",
+			args: args{
+				s: "",
+				p: "***",
 			},
-			{
-				name: "case 8",
-				args: args{
-					s: "a",
-					p: "***?",
-				},
-				want: true,
+			want: true,
+		},
+		{
+			name: "case 8",
+			args: args{
+				s: "a",
+				p: "***?",
 			},
-			{
-				name: "case 9",
-				args: args{
-					s: "a",
-					p: "**?**",
-				},
-				want: true,
+			want: true,
+		},
+		{
+			name: "case 9",
+			args: args{
+				s: "a",
+				p: "**?**",
 			},
-			{
-				name: "case 10",
-				args: args{
-					s: "",
-					p: "**?**",
-				},
-				want: false,
+			want: true,
+		},
+		{
+			name: "case 10",
+			args: args{
+				s: "",
+				p: "**?**",
 			},
-			{
-				name: "case 11",
-				args: args{
-					s: "aaabaababaafadfadbbaafadafadfadfbbbaaaaba",
-					p: "a****************************b",
-				},
-				want: false,
+			want: false,
+		},
+		{
+			name: "case 11",
+			args: args{
+				s: "aaabaababaafadfadbbaafadafadfadfbbbaaaaba",
+				p: "a****************************b",
 			},
-			{
-				name: "case 12",
-				args: args{
-					s: "babbbbaabababaabbababaababaabbaabababbaaababbababaaaaaabbabaaaabababbabbababbbaaaababbbabbbbbbbbbbaabbb",
-					p: "b**bb**a**bba*b**a*bbb**aba***babbb*aa****aabb*bbb***a*************",
-				},
-				want: false,
+			want: false,
+		},
+		{
+			name: "case 12",
+			args: args{
+				s: "babbbbaabababaabbababaababaabbaabababbaaababbababaaaaaabbabaaaabababbabbababbbaaaababbbabbbbbbbbbbaabbb",
+				p: "b**bb**a**bba*b**a*bbb**aba***babbb*aa****aabb*bbb***a*************",
 			},
-			{
-				name: "case 13",
-				args: args{
-					s: "aa",
-					p: "a*",
-				},
-				want: true,
+			want: false,
+		},
+		{
+			name: "case 13",
+			args: args{
+				s: "aa",
+				p: "a*",
 			},
-			{
-				name: "case 14",
-				args: args{
-					s: "aa",
-					p: "b*",
-				},
-				want: false,
+			want: true,
+		},
+		{
+			name: "case 14",
+			args: args{
+				s: "aa",
+				p: "b*",
 			},
-			{
-				name: "case 15",
-				args: args{
-					s: "aa",
-					p: "*a",
-				},
-				want: true,
+			want: false,
+		},
+		{
+			name: "case 15",
+			args: args{
+				s: "aa",
+				p: "*a",
 			},
-			{
-				name: "case 16",
-				args: args{
-					s: "aa",
-					p: "*b",
-				},
-				want: false,
+			want: true,
+		},
+		{
+			name: "case 16",
+			args: args{
+				s: "aa",
+				p: "*b",
 			},
-			{
-				name: "case 17",
-				args: args{
-					s: "mississippi",
-					p: "m*issip*",
-				},
-				want: true,
+			want: false,
+		},
+		{
+			name: "case 17",
+			args: args{
+				s: "mississippi",
+				p: "m*issip*",
 			},
-		*/
+			want: true,
+		},
+		
 		{
 			name: "case 18",
 			args: args{
 				s: "abaabaaaabbabbaaabaabababbaabaabbabaaaaabababbababaabbabaabbbbaabbbbbbbabaaabbaaaaabbaabbbaaaaabbbabb",
 				p: "ab*aaba**abbaaaa**b*b****aa***a*b**ba*a**ba*baaa*b*ab*",
 			},
-			want: true,
+			want: false,
 		},
 	}
 	for _, tt := range tests {
